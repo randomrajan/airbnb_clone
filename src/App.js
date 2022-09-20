9749bddae9a5ede90629d036f62825feb2bf9da7
theflaky <rajan2499@gmail.com>
2022-09-20 17:09:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import NavBar from "./components/NavBar"
 import Hero from "./components/Hero"
+import Card from "./components/Card"
 
 function App() {
   return (
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,19 @@
 import React from "react";
+import katiezaferes from './katiezaferes.png'
+import star from './star.png'
 
 export default function Card(){
     return(
-        <div>
-            
+        <div className="card">
+            <img src={katiezaferes} alt='katiezaferes' className="card-image"/>
+            <div className="card-stats">
+                <img src={star} alt='star'/>
+                <span>5.0</span>
+                <span>(6) • </span>
+                <span>USA</span>
+                <p>Life Lessons with Katie Zaferes</p>
+            <p>From $136 / person</p>
+            </div>
         </div>
     )
 }
