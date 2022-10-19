372bd26fe7211e3a7c3619d1ccbe4c76da9af570
theflaky <rajan2499@gmail.com>
2022-10-19 17:48:26 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,33 @@ import './App.css';
 import NavBar from "./components/NavBar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
-import katiezaferes from './katiezaferes.png'
-import weddingimage from './wedding-image.png'
+//import katiezaferes from './katiezaferes.png'
+//import weddingimage from './wedding-image.png'
 import star from './star.png'
+import data from './data.js'
+
+console.log(data)
 
 function App() {
+  
+  const cards = data.map(item => {
+    return (
+        <Card 
+            img={item.coverImg}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            location={item.location}
+            title={item.title}
+            price={item.price}
+        />
+    )
+}) 
+
   return (
     <div>
       <NavBar />
       <Hero />
-      <Card 
-          image = {katiezaferes} 
-          star= {star} 
-          rating = "5.0"
-          amount = "(6)"
-          dot = "•"
-          country = "USA"
-          title = "Life Lessons with Katie Zaferes"
-          money = "From $136"
-      />
+      {cards}
 
 
     </div>
